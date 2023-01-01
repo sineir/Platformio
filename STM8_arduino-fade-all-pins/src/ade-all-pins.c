62dcafc97a6c554a078c2f5affcc87353776591b
Sineir <sineir@sineir.com.br>
2023-01-01 18:41:05 -0300

    Pisca 7 ledes
    Alterado para um pisca-pisca tipo de bicicleta, para usar em carrinho de brinquedo.

--- STM8_arduino-fade-all-pins/src/ade-all-pins.c
+++ STM8_arduino-fade-all-pins/src/ade-all-pins.c
@@ -1,37 +1,103 @@
+#include <Arduino.h>
 /*
- * fade-all-pins
- *
- * This example shows how to fade an LED on all PWM-capable pins
- * using the analogWrite() function. (Derived from 01.Basics/Fade)
- *
- * This example code is in the public domain.
+ * Alterado 2023-01-01
+ * pisca 7 leds
  */
 
 #include <Arduino.h>
 
-int brightness = 0;    // how bright the LED is
-int fadeAmount = 5;    // how many points to fade the LED by
+int brightness = 0; // how bright the LED is
+int fadeAmount = 5; // how many points to fade the LED by
 char i;
-
+int leds[] = {8, 9, 5, 6, 12, 10, 11};
+int laps = 0;
 // the setup routine runs once when you press reset:
-void setup()  {
+void setup()
+{
   // declare all pin to be an output:
-  for (i=0; i<NUM_DIGITAL_PINS; i++) pinMode(i,OUTPUT);
-} 
+  // for (i = 0; i < NUM_DIGITAL_PINS; i++)
+  // pinMode(i, OUTPUT);
+
+  for (i = 8; i < 13; i++)
+    pinMode(i, OUTPUT);
+
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+}
 
 // the loop routine runs over and over again forever:
-void loop()  {
+void loop()
+{
+
   // set the brightness of all pins:
-  for (i=0; i<NUM_DIGITAL_PINS; i++) analogWrite(i,brightness);
+  analogWrite(5, brightness);
+  analogWrite(6, brightness);
+  analogWrite(12, brightness);
 
   // change the brightness for next time through the loop:
   brightness = brightness + fadeAmount;
 
-  // reverse the direction of the fading at the ends of the fade: 
-  if (brightness == 0 || brightness == 255) {
-    fadeAmount = -fadeAmount ; 
-  }     
-  // wait for 30 milliseconds to see the dimming effect    
-  delay(30);                            
-}
+  if (brightness % 51 == 0)
+  {
+    for (i = 8; i < 12; i++)
+    {
+      digitalWrite(i, !digitalRead(i));
+    }
+  }
 
+  // reverse the direction of the fading at the ends of the fade:
+  if (brightness == 0 || brightness == 255)
+  {
+    fadeAmount = -fadeAmount;
+    laps++;
+  }
+  // wait for 30 milliseconds to see the dimming effect
+  delay(7);
+
+  if (laps == 10)
+  {
+    laps = 0;
+    for (int kk = 0; kk < 10; kk++)
+    {
+
+      for (i = 0; i < 7; i++)
+        digitalWrite(leds[i], LOW);
+
+      for (int j = 0; j < 7; j++)
+      {
+        digitalWrite(leds[j], HIGH);
+        delay(40);
+      }
+
+      for (int j = 0; j < 7; j++)
+      {
+        digitalWrite(leds[j], LOW);
+        delay(40);
+      }
+    }
+
+    for (int kk = 0; kk < 10; kk++)
+    {
+
+      for (i = 0; i < 7; i++)
+        digitalWrite(leds[i], LOW);
+
+      for (int j = 6; j >= 0; j--)
+      {
+        digitalWrite(leds[j], HIGH);
+        delay(40);
+      }
+
+      for (int j = 6; j >= 0; j--)
+      {
+        digitalWrite(leds[j], LOW);
+        delay(40);
+      }
+    }
+
+    digitalWrite(8, HIGH);
+    digitalWrite(9, HIGH);
+  }
+}
